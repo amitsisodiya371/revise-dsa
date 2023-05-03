c82ca93dbe904547f012baf6ed6b561e1103d8e8
amitsisodiya371 <asisodiya9045@gmail.com>
2023-05-03 21:29:52 +0530

    target

--- DSA/Target sum.java
+++ DSA/Target sum.java
@@ -34,38 +34,71 @@ class Main {
 
     // 2 APPROACH leet code O(n)
     
-    public static void main(String[] args) {
-        int nums[] = { 2, 7, 11, 15 };
-        int target = 9;
-        // public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
-        int ans[] = new int[2];
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            if (map.containsKey(target - nums[i])) {
-                ans[1] = i;
-                ans[0] = map.get(target - nums[i]);
-                System.out.println(ans);
-            }
-            map.put(nums[i], i);
-        }
-        System.out.println(ans);
+    // public static void main(String[] args) {
+    //     int nums[] = { 2, 7, 11, 15 };
+    //     int target = 9;
+    //     // public int[] twoSum(int[] nums, int target) {
+    //     int n = nums.length;
+    //     int ans[] = new int[2];
+    //     Map<Integer, Integer> map = new HashMap<>();
+    //     for (int i = 0; i < n; i++) {
+    //         if (map.containsKey(target - nums[i])) {
+    //             ans[1] = i;
+    //             ans[0] = map.get(target - nums[i]);
+    //             System.out.println(ans);
+    //         }
+    //         map.put(nums[i], i);
+    //     }
+    //     System.out.println(ans);
 
-    }
+    // }
 
     // leet code o(n^2)
 
     // public static void main(String[] args) {
     //     int nums[] = { 2, 7, 11, 15 };
     //     int target = 9;
-    //     {
-    //         for (int i = 0; i < nums.length - 1; i++)
-    //             for (int j = i + 1; j < nums.length; j++) {
-    //                 if (nums[i] + nums[j] == target)
-    //                     System.out.println(i + " " + j);
-    //             }
+    //     boolean f = false;
+    //     for (int i = 0; i < nums.length - 1; i++){
+    //         for (int j = i + 1; j < nums.length; j++) {
+    //             if (nums[i] + nums[j] == target)
+    //                 System.out.println(nums[i] + " " + nums[j]);
+    //                 f= true;
+    //         } 
+    //     }
+    //     if(f==false){
     //         System.out.println(-1 + " " + -1);
     //     }
     // }
 
+
+    //two pointers o(n)
+
+    public static void main(String[] args){
+        int [] nums = {2, 17, 7, 12};
+        int t = 9;
+        int l =0;
+        int r =nums.length-1;
+        boolean f = false;
+        Arrays.sort(nums);
+        while(l<r){
+            for(int i=0;i<nums.length;i++)
+            if(nums[l]+nums[r]==t){
+                // System.out.println("yes");
+                f=true;
+            }else if(nums[l]+nums[r]>t){
+                r--;
+            }else{
+                l++;
+            }
+        }
+        if(f==true){
+            System.out.println("yes");  
+        }
+        else{
+            System.out.println("NO");   
+        }
+        // 
+        // System.out.println(nums[i]);
+    }
 }
